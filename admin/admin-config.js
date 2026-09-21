ecd5ddaec855693e18b2a462a6f117d14487b865
dalityuva <westbengaldalityuvamanch@gmail.com>
2026-09-22 01:54:57 +0530

    Update admin-config.js

--- admin/admin-config.js
+++ admin/admin-config.js
@@ -1,3 +1,4 @@
 // Put your Supabase Project URL and Publishable/anon key here.
 // NEVER put a service_role/secret key here.
-window.WBDYM_CONFIG={SUPABASE_URL:'https://YOUR-PROJECT.supabase.co',SUPABASE_ANON_KEY:'YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY'};
+window.WBDYM_CONFIG={SUPABASE_URL:'https://oahuhywputxkncddlmol.supabase.co/rest/v1/
+',SUPABASE_ANON_KEY:'sb_publishable_Me95pbzCTvG9kFV3frVXDw_l5YpafnG'};
